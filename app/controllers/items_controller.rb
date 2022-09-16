de6c9e5e438fb97283f166951ebbb8eb84c2ea42
pittan612 <taruchan0612pipi@gmail.com>
2022-09-16 17:54:47 +0900

    商品詳細機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  before_action :set_item, only: [:show]
   def index
     @items = Item.includes(:user).order('created_at DESC')    
   end
@@ -17,8 +19,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
