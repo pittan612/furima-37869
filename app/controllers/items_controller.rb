8bfc1046a4ecaa19631ada65a6f37d67d161ef5f
pittan612 <taruchan0612pipi@gmail.com>
2022-09-19 00:48:02 +0900

    削除機能の実装と見直し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,12 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show,]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
-  before_action :set_item, only: [:show, :edit, :update]
+  # ログインしていないユーザーはログインページに促す
+  before_action :authenticate_user!, except: [:index, :show]
+
+  # 重複処理をまとめる
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @items = Item.includes(:user).order('created_at DESC')    
+    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
@@ -20,31 +22,45 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
-
   def edit
-  end
-
-  def update
-    if @item.update(item_params)
-      redirect_to item_path(@item.id)
+    # ログインしているユーザーと同一であればeditファイルが読み込まれる
+    if @item.user_id == current_user.id 
     else
-      render :edit
+      redirect_to root_path
     end
   end
 
+  def update
+    @item.update(item_params)
+    # バリデーションがOKであれば詳細画面へ
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      # NGであれば、エラー内容とデータを保持したままeditファイルを読み込み、エラーメッセージを表示させる
+      render 'edit'
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    # ログインしているユーザーと同一であればデータを削除する
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
 
   private
+
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :item_status_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
   def set_item
     @item = Item.find(params[:id])
-  end
-  
-  def contributor_confirmation
-    redirect_to root_path unless current_user.id == @item.user.id
   end
 end
